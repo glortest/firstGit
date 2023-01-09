11a2d57bdb8c5bf108126fc49766dfa6ee302482
Gregory Omelchuk <gregoryomelchuk@vk.com>
2023-01-09 19:07:35 +0300

    edit Toast

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "t", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "abcdef", Toast.LENGTH_SHORT).show();
     }
 }
